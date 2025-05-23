2e7ffdf397a069417c97ed6b39b39a5ad6ba5fbd
nithin kumar <nithinkottary25@gmail.com>
2025-05-23 17:03:34 +0530

    fix logger timestamp globally

--- src/config/logger.ts
+++ src/config/logger.ts
@@ -4,33 +4,25 @@ import { Config } from '.'
 export const logger = winston.createLogger({
     level: 'info',
     defaultMeta: { service: 'auth-service' },
+    format: winston.format.combine(
+        winston.format.timestamp(),
+        winston.format.json(),
+    ),
     transports: [
         new winston.transports.File({
             level: 'info',
             dirname: 'logs',
             filename: 'combined.log',
-            format: winston.format.combine(
-                winston.format.timestamp(),
-                winston.format.json(),
-            ),
             silent: Config.NODE_ENV === 'test',
         }),
         new winston.transports.File({
             level: 'error',
             dirname: 'logs',
             filename: 'error.log',
-            format: winston.format.combine(
-                winston.format.timestamp(),
-                winston.format.json(),
-            ),
             silent: Config.NODE_ENV === 'test',
         }),
         new winston.transports.Console({
             level: 'info',
-            format: winston.format.combine(
-                winston.format.timestamp(),
-                winston.format.json(),
-            ),
             silent: Config.NODE_ENV === 'test',
         }),
     ],

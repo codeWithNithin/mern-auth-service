5a7ec605a7581df360465f30d90ffe59eaf6a86a
codeWithNithin <nithinkottary25@gmail.com>
2025-06-02 12:12:15 +0530

    tesing neon db sslmode

--- src/config/data-source.ts
+++ src/config/data-source.ts
@@ -13,7 +13,7 @@ export const AppDataSource = new DataSource({
     // because, if any of the entity changes, it will update the database directly.
     synchronize: false,
     logging: false,
-    // ssl: Config.DB_SSL === 'true',
+    ssl: Config.DB_SSL === 'true',
     entities: ['src/entity/*.{ts,js}'],
     migrations: ['src/migration/*.{ts,js}'],
     subscribers: [],

9198025c0530eaa945ce9cd969799b130bd78604
codeWithNithin <nithinkottary25@gmail.com>
2025-06-12 15:27:49 +0530

    some comments added

--- src/config/data-source.ts
+++ src/config/data-source.ts
@@ -2,6 +2,7 @@ import 'reflect-metadata'
 import { DataSource } from 'typeorm'
 import { Config } from '.'
 
+// product data source
 export const AppDataSource = new DataSource({
     type: 'postgres',
     host: Config.DB_HOST,
@@ -13,13 +14,13 @@ export const AppDataSource = new DataSource({
     // because, if any of the entity changes, it will update the database directly.
     synchronize: true,
     logging: false,
-    // ssl: true,
+    ssl: true,
     entities: ['src/entity/*.{ts,js}'],
     migrations: ['src/migration/*.{ts,js}'],
     subscribers: [],
-    // extra: {
-    //     ssl: {
-    //         rejectUnauthorized: false,
-    //     },
-    // },
+    extra: {
+        ssl: {
+            rejectUnauthorized: false,
+        },
+    },
 })
